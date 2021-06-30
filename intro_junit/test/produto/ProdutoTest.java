0b0faca8d4d6a9692d485cd26f7b2dbb523be811
Oliveira <Oliveira@DELL-RafaelOliveira>
2021-06-30 10:55:45 -0300

    Ajustes Test Produto

--- intro_junit/test/produto/ProdutoTest.java
+++ intro_junit/test/produto/ProdutoTest.java
@@ -11,6 +11,7 @@ import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ProdutoTest {
@@ -22,6 +23,7 @@ public class ProdutoTest {
 		livro = new Produto("Introdução ao Teste de Software", 100.00);
 	}
 	
+	@DisplayName("Teste Criar Produto")
 	@Test
 	public void testCriaProduto() {
 		Assertions.assertAll("livro",
@@ -30,20 +32,34 @@ public class ProdutoTest {
 				);
 	}
 	
+	@DisplayName("Teste Produtos Iguais")
 	@Test
 	public void testProdutosIguais() {
 		Produto livro2 = new Produto("Introdução ao Teste de Software", 90.00);
-		
 		assertNotSame(livro, livro2);
-		
 	}
 	
+	@DisplayName("Teste Valida Preco")
 	@Test
-	public void assertionComHamcrestMatcher() {
+	public void validaPreco() {
 		assertThat(livro.getPreco(), equalTo(100.00));
-		assertThat(livro.getNome(), notNullValue());
-		assertThat(livro.getNome(), containsString("Teste"));
-		assertThat(livro, instanceOf(Produto.class));
 	}
 
+	@DisplayName("Teste Valida Nome")
+	@Test
+	public void validaNome() {
+		assertThat(livro.getNome(), notNullValue());
+	}
+	
+	@DisplayName("Teste Valida Nome que Contem")
+	@Test
+	public void validaNomeContem() {
+		assertThat(livro.getNome(), containsString("Teste"));
+	}
+	
+	@DisplayName("Teste Valida Instancia")
+	@Test
+	public void validaInstancia() {
+		assertThat(livro, instanceOf(Produto.class));
+	}
 }
